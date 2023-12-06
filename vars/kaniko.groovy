1b768c75f7f55846c44528b05da6f05111547adb
Abdul Qavi <abdulqavi@ip-10-232-46-105.ec2.internal>
2023-12-06 16:13:55 -0500

    commented loggerdebug

--- vars/kaniko.groovy
+++ vars/kaniko.groovy
@@ -10,7 +10,7 @@ def push(Map properties=[:]) {
         ignorePathArg = ignorePathArg + "--ignore-path ${path} "
     }
     logger.info("Ignored Paths: ${ignorePathArg}")
-    logger.debug("executeKaniko params: ${properties}")
+    // logger.debug("executeKaniko params: ${properties}")
     logger.info("Docker User Args: ${properties.build.dockerargs}")
     // if (properties.build.dockerPath) {
     //     env.DOCKERPATH = "${properties.build.dockerPath}/"
