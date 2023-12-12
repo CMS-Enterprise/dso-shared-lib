ec55ea67485bfb0f6692a1aeb575a6b9d01c5e76
swaters23 <swaters@revacomm.com>
2023-12-12 09:56:57 -0600

    WIP - maven agent and groovy files added

---
+++ vars/maven.groovy
@@ -0,0 +1,23 @@
+def compile(Map buildArgs=[:]) {
+    withCredentials([file(credentialsId: "", variable: "FILE")]) {
+        sh "cp \$FILE /usr/share/maven/conf/settings.xml"
+    }
+    logger.info("Maven compiling")
+    logger.debug("buildArgs: ${buildArgs}")
+    buildParam = buildArgs?.buildParam ?: "clean compile"
+    sh "mvn ${buildParam}"
+}
+
+def test(Map testArgs[:]) {
+    logger.info("Maven testing")
+    logger.debug("testArgs: ${testArgs}")
+    testParam = testArgs?.testParam ?: "clean test"
+    sh "mvn ${testParam}"
+}
+
+def mvnPackage(Map packageArgs[:]) {
+    logger.info("Maven packaging")
+    logger.debug("packageArgs: ${packageArgs}")
+    packageParam = packageArgs?.packageParam ?: "clean package"
+    sh "mvn ${packageParam}"
+}
