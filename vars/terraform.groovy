64ba00ce1c78dcf5457ee3368b72151ffc69123a
swaters23 <swaters@revacomm.com>
2023-12-20 09:30:05 -0600

    Added terrform agent, groovy

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,7 +1,4 @@
 def init(Map deployArgs=[:]) {
-    // withCredentials([file(credentialsId: "", variable: "FILE")]) {
-    //     sh "cp \$FILE /usr/share/maven/conf/settings.xml"
-    // }
     logger.info("terraform init ")
     logger.debug("deployArgs: ${deployArgs}")
     sh "terraform init -backend-config=${deployArgs.backendConfigFile}"
@@ -10,11 +7,31 @@ def init(Map deployArgs=[:]) {
 def plan(Map deployArgs=[:]) {
     logger.info("terraform plan")
     logger.debug("deployArgs: ${deployArgs}")
-    sh "terraform plan -var-file=${deployArgs.tfVar}" 
+    sh "aws sts get-caller-identity"
+    AWSCRED = sh (script: "aws sts assume-role --role-arn ${deployArgs.awsRoleArn} --role-session-name AWSCLI-Session", returnStdout: true)
+    AWSCRED = readJSON text: "${AWSCRED}"
+    sh """
+        export AWS_ACCESS_KEY_ID=${AWSCRED.Credentials.AccessKeyId}
+        export AWS_SECRET_ACCESS_KEY=${AWSCRED.Credentials.SecretAccessKey}
+        export AWS_SESSION_TOKEN=${AWSCRED.Credentials.SessionToken}
+        aws sts get-caller-identity
+        terraform plan -var-file=${deployArgs.tfVar}
+    """
 }
 
 def apply(Map deployArgs=[:]) {
     logger.info("terraform apply")
     logger.debug("deployArgs: ${deployArgs}")
-    sh "terraform apply -var-file=${deployArgs.tfVar}"
+    sh "aws sts get-caller-identity"
+    AWSCRED = sh (script: "aws sts assume-role --role-arn ${deployArgs.awsRoleArn} --role-session-name AWSCLI-Session", returnStdout: true)
+    AWSCRED = readJSON text: "${AWSCRED}"
+    sh """
+        export AWS_ACCESS_KEY_ID=${AWSCRED.Credentials.AccessKeyId}
+        export AWS_SECRET_ACCESS_KEY=${AWSCRED.Credentials.SecretAccessKey}
+        export AWS_SESSION_TOKEN=${AWSCRED.Credentials.SessionToken}
+        aws sts get-caller-identity
+        terraform apply -var-file=${deployArgs.tfVar} --auto-approve
+        sleep 300
+        terraform destroy -var-file=${deployArgs.tfVar} --auto-approve
+    """
 }
