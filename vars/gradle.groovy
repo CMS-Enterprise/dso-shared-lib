3ba7df67d9116054f44a5b310fa93d9fc6836a44
swaters23 <swaters@revacomm.com>
2024-01-05 09:48:13 -0600

    checking gradle version

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -1,5 +1,8 @@
 def gradleBuild(String buildArgs="") {
     logger.info("Gradle build")
     logger.debug("buildArgs: ${buildArgs}")
-    sh "./gradlew ${buildArgs}"
+    sh """
+    gradle --version
+    ./gradlew ${buildArgs}
+    """
 }
