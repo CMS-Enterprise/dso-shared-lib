a4b45a1d104b4f424e2def66e7aa60468d6df1d0
swaters23 <swaters@revacomm.com>
2024-01-08 09:28:54 -0600

    renamed gradle8.5 -> gradle-java11

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -2,5 +2,35 @@ def gradleBuild(String buildArgs="") {
     logger.info("Gradle build")
     logger.debug("buildArgs: ${buildArgs}")
     buildParam = buildArgs?: "clean build"
-    sh "./gradlew ${buildArgs}"
+    sh "chmod +x gradlew; ./gradlew ${buildArgs}"
+}
+
+def gradleTest(String testArgs="") {
+    logger.info("Gradle test")
+    logger.debug("testArgs: ${testArgs}")
+    testParam = testArgs?: "test"
+    sh "chmod +x gradlew; ./gradlew ${testArgs}"
+}
+
+def setGradlePropsFile() {
+    logger.debug("Set Gradle Properties...")
+    //create gradle.properties
+    if (!fileExists('gradle.properties')) {
+        def gradleProps = libraryResource "gradle/gradle.properties"
+        writeFile file: "./gradle.properties", text: gradleProps
+    }
+    //inject auth creds into gradle.properties
+    withCredentials([usernamePassword(credentialsId: "${env.artifactoryUsernamePasswordlId}", usernameVariable: 'artifactoryUser', passwordVariable: 'artifactoryPassword')]) {
+        sh """
+            sed -i 's/\$artifactory_User/'${artifactoryUser}'/g' gradle.properties;
+            sed -i 's/\$artifactory_Pwd/'${artifactoryPassword}'/g' gradle.properties;
+        """
+    }
+}
+
+def removeGradlePropsFile() {
+    logger.debug("Remove Gradle Properties...")
+    if (fileExists('gradle.properties')) {
+        sh "rm -f gradle.properties;"
+    }
 }
