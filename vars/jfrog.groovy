549019f3db01ab14f9283b37d5cf774cf136b08a
swaters23 <swaters@revacomm.com>
2024-01-11 09:49:12 -0600

    added 'xray' to the jfrog URL

--- vars/jfrog.groovy
+++ vars/jfrog.groovy
@@ -3,7 +3,7 @@ def jfrogXray(Map properties=[:]) {
     withCredentials([string(credentialsId: "JfrogArt-SA-ro-Token", variable: "TOKEN")]) {
         // Need to ask for their artifactory project and separate the artfactory rerpo and path names in {properties.build.artifactoryPath}?
         sh"""
-            jf build-scan --url=https://artifactory.cloud.cms.gov/ --access-token=${TOKEN} --project=${properties.artifactoryProjectName} --fail=false ${properties.artifactName} ${env.GIT_COMMIT}
+            jf build-scan --url=https://artifactory.cloud.cms.gov/xray --access-token=${TOKEN} --project=${properties.artifactoryProjectName} --fail=false ${properties.artifactName} ${env.GIT_COMMIT}
         """
     }
 }
