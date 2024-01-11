d5093000e8ba75fbbd35437635ea6e738ec016e0
swaters23 <swaters@revacomm.com>
2024-01-11 10:58:35 -0600

    switch case with full command

--- vars/snyk.groovy
+++ vars/snyk.groovy
@@ -8,13 +8,49 @@ def snykCodeTest(Map snykCodeTestArgs=[:]) {
 def snykTest(Map snykTestArgs=[:]) {
     logger.info("Snyk Test")
     logger.debug("snykTestArgs: ${snykTestArgs}")
-    gradleBuildPathParam = snykTestArgs.snyk.gradleBuildPath?: "build.gradle"
-    withCredentials([string(credentialsId: "snyk-sa-token", variable: "TOKEN")]) {
-        sh """
-            snyk auth ${TOKEN}
-            snyk test --org=${snykTestArgs.snyk.orgId} --file=${WORKSPACE}/${gradleBuildPathParam} --json
-        """
+    logger.info("Technology: ${snykTestArgs.tech}")
+    switch(snykTestArgs.tech) {
+        case snykTestArgs.tech==="gradle":
+            gradleBuildPathParam = snykTestArgs.snyk.gradleBuildPath?: "build.gradle"
+            withCredentials([string(credentialsId: "snyk-sa-token", variable: "TOKEN")]) {
+                sh """
+                    snyk auth ${TOKEN}
+                    snyk test --org=${snykTestArgs.snyk.orgId} --file=${WORKSPACE}/${gradleBuildPathParam} --json
+                """
+            }
+            break;
+        default:
+            withCredentials([string(credentialsId: "snyk-sa-token", variable: "TOKEN")]) {
+                sh """
+                    snyk auth ${TOKEN}
+                    snyk test --org=${snykTestArgs.snyk.orgId} --json
+                """
+            }
+            break;
     }
+}
+
+def snykTest2(Map snykTestArgs=[:]) {
+    logger.info("Snyk Test")
+    logger.debug("snykTestArgs: ${snykTestArgs}")
+    logger.info("Technology: ${snykTestArgs.tech}")
+    def xrayCommand = "snyk test --org=${snykTestArgs.snyk.orgId} --json"
+    switch(snykTestArgs.tech) {
+        case snykTestArgs.tech==="gradle":
+            gradleBuildPathParam = snykTestArgs.snyk.gradleBuildPath?: "build.gradle"
+            xrayCommand += " --file=${WORKSPACE}/${gradleBuildPathParam}"
+            break;
+        default:
+            break;
+    }
+    withCredentials([string(credentialsId: "snyk-sa-token", variable: "TOKEN")]) {
+    sh """
+        snyk auth ${TOKEN}
+        ${xrayCommand}
+    """
+    }
+
+
 }
 
 def snykMonitor(Map snykMonitorArgs=[:]) {
