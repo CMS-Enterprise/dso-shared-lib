e113d70279abdab571103d309c396ba07d498b7f
swaters23 <swaters@revacomm.com>
2024-01-19 09:04:51 -0600

    added wget on jenkinsUpdateToken

--- vars/cloudbees.groovy
+++ vars/cloudbees.groovy
@@ -17,6 +17,7 @@ def jenkinsUpdateToken(String refreshedToken) {
     logger.info("Update Access Token Credentials")
     withCredentials(string[credentialsId: 'jenkins-svc-api-token', variable:'TOKEN'])
     sh """
+        yum install -y wget
         wget -i https://jenkins-dev-west.cloud.cms.gov/cjoc/jnlpJars/jenkins-cli.jar
         java -jar jenkins-cli.jar -s https://jenkins-dev-west.cloud.cms.gov/ -http -auth dso-jenkins-dev-a:${TOKEN} 
             update-credentials-by-xml system::system::jenkins _ JfrogArt-SA-ro-Token < updated_jfrog_token.xml
