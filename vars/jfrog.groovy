b7fba9d04e1ad1b3c5199dc0de7a64ba0d6c9676
swaters23 <swaters@revacomm.com>
2024-01-22 09:41:57 -0600

    remove double quotes from refreshedToken

--- vars/jfrog.groovy
+++ vars/jfrog.groovy
@@ -27,7 +27,7 @@ def jfrogRefreshToken(String refreshedToken) {
             apk add --no-cache bash jq
             jq --version
             refreshedTokenResponse=$(jf rt access-token-create --url=https://artifactory.cloud.cms.gov/artifactory --user=${USER} --password=${PASS} --groups=Admins --expiry=3456000)
-            refreshedToken=$(echo ${refreshedTokenResponse} | jq .access_token)
+            refreshedToken=$(echo ${refreshedTokenResponse} | jq .access_token | tr -d '"' '')
             testVar="<secret>${refreshedToken}<\\/secret>"
             ls
             sed -i "" 's|<secret>.*<\\/secret>|'${testVar}'|g;' update-string-cred.xml
