7ef0716ee3da370715a322d65099b2346adbfd4f
swaters23 <swaters@revacomm.com>
2024-02-09 11:58:33 -0600

    jfrgo config enc-password=false

--- vars/jfrog.groovy
+++ vars/jfrog.groovy
@@ -16,7 +16,7 @@ def jfrogXray(Map properties=[:]) {
     withCredentials([usernamePassword(credentialsId: "jfrog-prod-sa", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
         // jf build-scan --url=https://artifactory.cloud.cms.gov/xray --access-token=${TOKEN} --project=${properties.artifactoryProjectName} --fail=false ${properties.artifactName} ${env.GIT_COMMIT}
         sh"""
-            jf c add cms-artifactory --url=artifactory.cloud.cms.gov --user=${USERNAME} --password=${PASSWORD}
+            jf c add cms-artifactory --url=artifactory.cloud.cms.gov --user=${USERNAME} --password=${PASSWORD} --enc-password=false
             jf c show
             jf xr curl '/api/v1/artifacts?search=${properties.artifactName}/${env.GIT_COMMIT}/manifest.json&repo=${properties.build.repoName}'
         """
