0d5b621dcb78bdfcf52959483b3ce9752416a6b0
swaters23 <swaters@revacomm.com>
2024-02-09 11:29:18 -0600

    using jfrog-prod-sa

--- vars/jfrog.groovy
+++ vars/jfrog.groovy
@@ -12,12 +12,13 @@ def jfrogPublishBuild(Map properties=[:]) {
 def jfrogXray(Map properties=[:]) {
     logger.info("JFrog XRay Scan")
     // jfrogPublishBuild(properties)
-    withCredentials([string(credentialsId: "JfrogArt-SA-ro-Token", variable: "TOKEN")]) {
+    // withCredentials([string(credentialsId: "JfrogArt-SA-ro-Token", variable: "TOKEN")]) {
+    withCredentials([usernamePassword(credentialsId: "jfrog-prod-sa", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')])
         // jf build-scan --url=https://artifactory.cloud.cms.gov/xray --access-token=${TOKEN} --project=${properties.artifactoryProjectName} --fail=false ${properties.artifactName} ${env.GIT_COMMIT}
         sh"""
-            jf c add --url=artifactory.cloud.cms.gov --access-token=${TOKEN}
+            jf c add --url=artifactory.cloud.cms.gov --user=${USERNAME} --password=${PASSWORD}
             jf c show
-            jf xr curl '/api/v1/artifacts?search=${properties.build.artifactName}/${env.GIT_COMMIT}/manifest.json&repo=${properties.build.repoName}'
+            jf xr curl '/api/v1/artifacts?search=${properties.artifactName}/${env.GIT_COMMIT}/manifest.json&repo=${properties.build.repoName}'
         """
     }
 }
