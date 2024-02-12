b061c5b64c33ba78f80d3974a9cfe33f256fb6af
swaters23 <swaters@revacomm.com>
2024-02-12 10:43:52 -0500

    triple single quote shell script

--- vars/jfrog.groovy
+++ vars/jfrog.groovy
@@ -15,13 +15,13 @@ def jfrogXray(Map properties=[:]) {
     withCredentials([string(credentialsId: "JfrogArt-SA-ro-Token", variable: "TOKEN")]) {
         // jf build-scan --url=https://artifactory.cloud.cms.gov/xray --access-token=${TOKEN} --project=${properties.artifactoryProjectName} --fail=false ${properties.artifactName} ${env.GIT_COMMIT}
         // def repoName=artifactoryPath.split("/")[0]
-        sh"""
+        sh'''
             jf c add cms-artifactory --url=https://artifactory.cloud.cms.gov/ --access-token=${TOKEN}
             jf c show
-            xrayResponse=$(jf xr curl '/api/v1/artifacts?search=${properties.artifactName}/${env.GIT_COMMIT}/manifest.json&repo=${repoName}')
+            xrayResponse=$(jf xr curl "/api/v1/artifacts?search=${properties.artifactName}/${env.GIT_COMMIT}/manifest.json&repo=${repoName}")
             apk add --no-cache bash jq
-            echo "${xrayResponse}" | jq
-        """
+            echo ${xrayResponse} | jq
+        '''
     }
 }
 
