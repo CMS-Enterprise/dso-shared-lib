038d32e32ba25918d5fea6572b2b315d748e0663
Jason Castonguay <jcastonguay>
2024-02-13 14:23:06 -0500

    added terraform test

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -6,7 +6,7 @@ def init(Map deployArgs=[:]) {
         git clone https://github.com/tfutils/tfenv.git ~/.tfenv
         echo "export PATH="$HOME/.tfenv/bin:$PATH" >> ~/.profile
         echo "trust-tfenv: yes" > ~/.tfenv/use-gpgv
-        terraform -chdir=${deployArgs.backendConfigFile} init -backend-config=${deployArgs.backendConfigFile}
+        terraform -chdir=${deployArgs.deploy.backendConfigFile} init -backend-config=${deployArgs.deploy.backendConfigFile}
     """
 }
 
@@ -14,7 +14,7 @@ def plan(Map deployArgs=[:]) {
     logger.info("terraform plan")
     logger.debug("deployArgs: ${deployArgs}")
     sh "aws sts get-caller-identity"
-    AWSCRED = sh (script: "aws sts assume-role --role-arn ${deployArgs.awsRoleArn} --role-session-name AWSCLI-Session", returnStdout: true)
+    AWSCRED = sh (script: "aws sts assume-role --role-arn ${deployArgs.deploy.awsRoleArn} --role-session-name AWSCLI-Session", returnStdout: true)
     AWSCRED = readJSON text: "${AWSCRED}"
     sh """
         export AWS_ACCESS_KEY_ID=${AWSCRED.Credentials.AccessKeyId}
@@ -22,15 +22,33 @@ def plan(Map deployArgs=[:]) {
         export AWS_SESSION_TOKEN=${AWSCRED.Credentials.SessionToken}
         unset AWS_WEB_IDENTITY_TOKEN_FILE
         aws sts get-caller-identity
-        terraform -chdir=${WORKSPACE}/${deployArgs.backendConfigFile} plan -var-file=${WORKSPACE}/${deployArgs.tfVar} 
+        terraform -chdir=${WORKSPACE}/${deployArgs.deploy.backendConfigFile} plan -var-file=${WORKSPACE}/${deployArgs.deploy.tfVar} ${deployArgs.buildArgs}
     """
+}
+
+def test(Map deployArgs=[:]) {
+    logger.info("terraform test")
+    logger.debug("deployArgs: ${deployArgs}")
+    sh "aws sts get-caller-identity"
+    AWSCRED = sh (script: "aws sts assume-role --role-arn ${deployArgs.deploy.awsRoleArn} --role-session-name AWSCLI-Session", returnStdout: true)
+    AWSCRED = readJSON text: "${AWSCRED}"
+    sh """
+        export AWS_ACCESS_KEY_ID=${AWSCRED.Credentials.AccessKeyId}
+        export AWS_SECRET_ACCESS_KEY=${AWSCRED.Credentials.SecretAccessKey}
+        export AWS_SESSION_TOKEN=${AWSCRED.Credentials.SessionToken}
+        unset AWS_WEB_IDENTITY_TOKEN_FILE
+        aws sts get-caller-identity
+        terraform -chdir=${WORKSPACE}/${deployArgs.deploy.backendConfigFile} fmt -check -var-file=${WORKSPACE}/${deployArgs.deploy.tfVar} ${deployArgs.testArgs}
+        terraform -chdir=${WORKSPACE}/${deployArgs.deploy.backendConfigFile} validate -var-file=${WORKSPACE}/${deployArgs.deploy.tfVar} ${deployArgs.testArgs}
+        terraform -chdir=${WORKSPACE}/${deployArgs.deploy.backendConfigFile} test -var-file=${WORKSPACE}/${deployArgs.deploy.tfVar} ${deployArgs.testArgs}
+      """
 }
 
 def apply(Map deployArgs=[:]) {
     logger.info("terraform apply")
     logger.debug("deployArgs: ${deployArgs}")
     sh "aws sts get-caller-identity"
-    AWSCRED = sh (script: "aws sts assume-role --role-arn ${deployArgs.awsRoleArn} --role-session-name AWSCLI-Session", returnStdout: true)
+    AWSCRED = sh (script: "aws sts assume-role --role-arn ${deployArgs.deploy.awsRoleArn} --role-session-name AWSCLI-Session", returnStdout: true)
     AWSCRED = readJSON text: "${AWSCRED}"
     // Current command also contains "sleep" and "destroy" command for testing purposes
     // TODO: Remove before release
@@ -40,8 +58,8 @@ def apply(Map deployArgs=[:]) {
         export AWS_SESSION_TOKEN=${AWSCRED.Credentials.SessionToken}
         unset AWS_WEB_IDENTITY_TOKEN_FILE
         aws sts get-caller-identity
-        terraform -chdir=${WORKSPACE}/${deployArgs.backendConfigFile} apply -var-file=${WORKSPACE}/${deployArgs.tfVar} --auto-approve
+        terraform -chdir=${WORKSPACE}/${deployArgs.deploy.backendConfigFile} apply -var-file=${WORKSPACE}/${deployArgs.deploy.tfVar} ${deployArgs.buildArgs}--auto-approve
         sleep 30
-        terraform -chdir=${WORKSPACE}/${deployArgs.backendConfigFile} destroy -var-file=${WORKSPACE}/${deployArgs.tfVar} --auto-approve
+        terraform -chdir=${WORKSPACE}/${deployArgs.deploy.backendConfigFile} destroy -var-file=${WORKSPACE}/${deployArgs.deploy.tfVar} --auto-approve
     """
 }
