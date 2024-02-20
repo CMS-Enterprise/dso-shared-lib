d836c1207acc9a65b41f3907b6094c0a6313a51a
Abdul Qavi <abdulqavi@ip-10-232-42-13.ec2.internal>
2024-02-20 10:17:23 -0500

    added aws cred stage for kaniko-ECR

---
+++ vars/aws.groovy
@@ -0,0 +1,19 @@
+def assumeRole(String ADOIAMRole) {
+    sh '''
+        aws sts assume-role \
+        --role-arn ${ADOIAMRole} \
+        --role-session-name session \
+        --output text \
+        --query Credentials \
+        > /tmp/role-creds.txt
+
+        aws sts get-caller-identity
+
+        cat > .aws-creds <<EOF
+[default]
+aws_access_key_id = $(cut -f1 /tmp/role-creds.txt)
+aws_secret_access_key = $(cut -f3 /tmp/role-creds.txt)
+aws_session_token = $(cut -f4 /tmp/role-creds.txt)
+EOF
+    '''
+}
