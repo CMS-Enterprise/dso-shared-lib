eaf0ff240e5c4b37dcdd1b55d201cdd61472d972
Abdul Qavi <abdulqavi@ip-10-232-49-182.ec2.internal>
2024-02-20 13:28:49 -0500

    addressing pipeline 153 error

--- vars/aws.groovy
+++ vars/aws.groovy
@@ -1,8 +1,8 @@
 def assumeRole(String ADOIAMRole) {
-    sh """
+    sh '''
         echo "Role ARN: ${ADOIAMRole}"
         aws sts assume-role \
-        --role-arn ${ADOIAMRole} \
+        --role-arn "$${ADOIAMRole}" \
         --role-session-name session \
         --output text \
         --query Credentials \
@@ -16,5 +16,5 @@ aws_access_key_id = $(cut -f1 /tmp/role-creds.txt)
 aws_secret_access_key = $(cut -f3 /tmp/role-creds.txt)
 aws_session_token = $(cut -f4 /tmp/role-creds.txt)
 EOF
-    """
+    '''
 }
