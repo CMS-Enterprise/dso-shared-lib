afdec45900de0a9bbc7ac6217f2943519cfe7ebb
Abdul Qavi <abdulqavi@ip-10-232-43-55.ec2.internal>
2024-02-21 12:05:49 -0500

    update 10

--- vars/aws.groovy
+++ vars/aws.groovy
@@ -1,15 +1,24 @@
-def assumeRole(String ADOIAMRole="x") {
-    sh '''
-        echo "Role ARN: ${ADOIAMRole}"
-        aws sts assume-role --role-arn ${ADOIAMRole} --role-session-name session --output text --query Credentials > /tmp/role-creds.txt
-
-        aws sts get-caller-identity
-
-        cat > .aws-creds <<EOF
+def assumeRole="ADOIAMRole" 
+withEnv(["RoleARN=$assumeRole"]) {
+        sh '''
+              aws sts assume-role \
+                --role-arn $RoleARN \
+                --role-session-name session \
+                --output text \
+                --query Credentials \
+                > /tmp/role-creds.txt
+    
+              cat > .aws-creds <<EOF
 [default]
 aws_access_key_id = $(cut -f1 /tmp/role-creds.txt)
 aws_secret_access_key = $(cut -f3 /tmp/role-creds.txt)
 aws_session_token = $(cut -f4 /tmp/role-creds.txt)
 EOF
-    '''
+    
+                cp -v .aws-creds $HOME/.aws/credentials
+                unset AWS_WEB_IDENTITY_TOKEN_FILE
+                
+                 aws sts get-caller-identity
+             
+           '''
 }
