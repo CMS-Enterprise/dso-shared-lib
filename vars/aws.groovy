89704aebc18236e596acd257692c036e8c6a0aec
Abdul Qavi <abdulqavi@ip-10-232-51-82.ec2.internal>
2024-02-27 10:23:45 -0500

    updated aws groovy

--- vars/aws.groovy
+++ vars/aws.groovy
@@ -15,7 +15,7 @@ aws_access_key_id = $(cut -f1 /tmp/role-creds.txt)
 aws_secret_access_key = $(cut -f3 /tmp/role-creds.txt)
 aws_session_token = $(cut -f4 /tmp/role-creds.txt)
 EOF 
-          
+            cat .aws-creds
             aws sts get-caller-identity
         
         '''
