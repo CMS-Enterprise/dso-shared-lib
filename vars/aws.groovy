7bb6d1eccbb9dfee8cf678c933e09be28966c58e
Abdul Qavi <abdulqavi@ip-10-232-51-82.ec2.internal>
2024-02-27 10:31:49 -0500

    updated aws groovy 02

--- vars/aws.groovy
+++ vars/aws.groovy
@@ -8,15 +8,15 @@ def assumeRole(String ADOIAMRole)  {
               --output text \
               --query Credentials \
               > /tmp/role-creds.txt
-  
-            cat > .aws-creds <<EOF
-[default]
-aws_access_key_id = $(cut -f1 /tmp/role-creds.txt)
-aws_secret_access_key = $(cut -f3 /tmp/role-creds.txt)
-aws_session_token = $(cut -f4 /tmp/role-creds.txt)
-EOF 
+
+            echo [default] >> .aws-creds
+            echo  aws_access_key_id = $(cut -f1 /tmp/role-creds.txt)  >> .aws-creds
+            echo  aws_secret_access_key = $(cut -f3 /tmp/role-creds.txt)  >> .aws-creds
+            echo  aws_session_token = $(cut -f4 /tmp/role-creds.txt)  >> .aws-creds
+            
             cat .aws-creds
             aws sts get-caller-identity
+
         
         '''
           }
