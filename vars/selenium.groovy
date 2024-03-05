ba58f8ca7d364891692cfe91f7e965efa8d666c6
swaters23 <swaters@revacomm.com>
2024-03-05 11:57:01 -0600

    Updated selenium script

--- vars/selenium.groovy
+++ vars/selenium.groovy
@@ -1,33 +1,30 @@
 def runSeleniumTests(Map seleniumTestArgs=[:]) {
     logger.info("Starting Selenium Tests")
-    logger.debug("seleniumTestArgs: ${seleniumTestArgs}")
-    if (!seleniumTestArgs.containsKey('testUrl')) {
-        throw new IllegalArgumentException("testUrl is a mandatory parameter for runSeleniumTests.")
+    logger.debug("seleniumTestArgs: ${seleniumTestArgs.selenium}")
+    if (!seleniumTestArgs.containsKey("testUrl")) {
+        throw new IllegalArgumentException("testUrl is a mandatory parameter for Selenium testing.")
     }
     
-    def testUrl = seleniumTestArgs.get('testUrl')
-    def browser = seleniumTestArgs.get('browser', 'chrome')
-    def video = seleniumTestArgs.get('video', 'false')
-    def credentialsId = seleniumTestArgs.get('credentialsId', 'sbox')
-    def mavenSettingsFileId = seleniumTestArgs.get('mavenSettingsFileId', 'b14b5104-8739-4627-9fd8-69b8b323fb6e')
+    def testUrl = seleniumTestArgs.get("testUrl")
+    def browser = seleniumTestArgs.get("browser", "chrome")
+    def video = seleniumTestArgs.get("video", "false")
+    // I have questions about the below 2 vars
+    def credentialsId = seleniumTestArgs.get("credentialsId", "selenium-access-token")
+    def mavenConfigFileId = seleniumTestArgs.get("mavenConfigFileId")
 
-    logger.info("Technology: Maven")
+    logger.info("Technology: ${seleniumTestArgs.tech}")
     logger.info("Test URL: ${testUrl}")
     logger.info("Browser: ${browser}")
     logger.info("Video Enabled: ${video}")
 
-    def mvnCommand = "mvn -s \$MAVEN_SETTINGS clean test -Ds.url=${testUrl} -Ds.token=\$TOKEN -Ds.browser=${browser} -Ds.video=${video}"
+    def mvnCommand = "mvn -s ${mavenConfigFileId} clean test -Ds.url=${testUrl} -Ds.token=\$TOKEN -Ds.browser=${browser} -Ds.video=${video}"
 
-    
-    withCredentials([string(credentialsId: credentialsId, variable: 'TOKEN')]) {
-        configFileProvider([configFile(fileId: mavenSettingsFileId, variable: 'MAVEN_SETTINGS')]) {
-    sh """
-        cd test/maven
-        ${mvnCommand}
-    """
-        }
+    withCredentials([string(credentialsId: "${credentialsId}", variable: "TOKEN")]) {
+        sh """
+            cd test/maven
+            ${mvnCommand}
+        """
     }
-    
     
     logger.info("Selenium Tests Completed")
 }
