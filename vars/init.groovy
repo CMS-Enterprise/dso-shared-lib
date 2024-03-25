e078a20bca33b76e57867703fe4269ac7dc8ae49
swaters23 <swaters@revacomm.com>
2024-03-25 09:33:46 -0500

    fixed init

--- vars/init.groovy
+++ vars/init.groovy
@@ -18,7 +18,7 @@ def paramValidator(Map properties=[:]) {
         logger.info("Zip file artifacts cannot be uploaded to ECR. Please provide an image-based artifact.")
         failure+=1
     }
-    if(!properties.build.dockerfile.trim() && !properties.build.fileName.trim()) {
+    if(properties.build.dockerfile && properties.build.fileName) {
         logger.info("Dockerfile and Zip file provided. Please choose 1 packaging type for upload.")
         failure+=1
     }
