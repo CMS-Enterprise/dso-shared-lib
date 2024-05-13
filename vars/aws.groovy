c4d0622f15f3a9988311ef29064dedf4f276940c
swaters23 <swaters@revacomm.com>
2024-05-13 09:59:01 -0500

    cleaning up aws.groovy - removed echo statements so auth creds don't print to pipeline

--- vars/aws.groovy
+++ vars/aws.groovy
@@ -3,6 +3,7 @@ def assumeRole(String ADOIAMRole)  {
     logger.info("No IAM Role defined")
     return
   } else {
+    logger.info("Assuming AWS role...")
     withEnv(["iamrole=$ADOIAMRole"]) {
       sh '''
         aws sts assume-role \
@@ -12,10 +13,10 @@ def assumeRole(String ADOIAMRole)  {
           --query Credentials \
           > /tmp/role-creds.txt
 
-        echo [default] >> .aws-creds
-        echo  aws_access_key_id = $(cut -f1 /tmp/role-creds.txt)  >> .aws-creds
-        echo  aws_secret_access_key = $(cut -f3 /tmp/role-creds.txt)  >> .aws-creds
-        echo  aws_session_token = $(cut -f4 /tmp/role-creds.txt)  >> .aws-creds
+        [default] >> .aws-creds
+        aws_access_key_id=$(cut -f1 /tmp/role-creds.txt)  >> .aws-creds
+        aws_secret_access_key=$(cut -f3 /tmp/role-creds.txt)  >> .aws-creds
+        aws_session_token=$(cut -f4 /tmp/role-creds.txt)  >> .aws-creds
         
         cp -v .aws-creds $HOME/.aws/credentials
         unset AWS_WEB_IDENTITY_TOKEN_FILE
