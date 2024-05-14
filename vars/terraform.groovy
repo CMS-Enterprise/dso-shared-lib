16bd116ff6e603673742f2e3145e58f35ae3d434
SJKQ-DSO <abdulqavi.shaik@cms.hhs.gov>
2024-05-14 13:58:40 -0400

    masking aws keys

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -11,9 +11,11 @@ def plan(Map deployArgs=[:]) {
     AWSCRED = sh (script: "aws sts assume-role --role-arn ${deployArgs.awsRoleArn} --role-session-name AWSCLI-Session", returnStdout: true)
     AWSCRED = readJSON text: "${AWSCRED}"
     sh """
+        set +x
         export AWS_ACCESS_KEY_ID=${AWSCRED.Credentials.AccessKeyId}
         export AWS_SECRET_ACCESS_KEY=${AWSCRED.Credentials.SecretAccessKey}
         export AWS_SESSION_TOKEN=${AWSCRED.Credentials.SessionToken
+        set +x
         aws sts get-caller-identity
         terraform -chdir=${WORKSPACE}/${deployArgs.backendConfigFile} plan -var-file=${WORKSPACE}/${deployArgs.tfVar} -no-color
     """
@@ -28,9 +30,11 @@ def apply(Map deployArgs=[:]) {
     // Current command also contains "sleep" and "destroy" command for testing purposes
     // TODO: Remove before release
     sh """
+        set +x
         export AWS_ACCESS_KEY_ID=${AWSCRED.Credentials.AccessKeyId}
         export AWS_SECRET_ACCESS_KEY=${AWSCRED.Credentials.SecretAccessKey}
         export AWS_SESSION_TOKEN=${AWSCRED.Credentials.SessionToken}
+        set +x
         aws sts get-caller-identity
         terraform -chdir=${WORKSPACE}/${deployArgs.backendConfigFile} apply -var-file=${WORKSPACE}/${deployArgs.tfVar} --auto-approve -no-color
         sleep 30
