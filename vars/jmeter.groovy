50db19f866e86807bd25f9db3ce64e678bf96610
swaters23 <swaters@revacomm.com>
2024-08-08 13:20:15 -0500

    intial jmeter script- put on hold for discussion

--- vars/jmeter.groovy
+++ vars/jmeter.groovy
@@ -1,7 +1,7 @@
 def jmeterTest(Map testArgs=[:]) {
     logger.info("Running JMeter tests")
     sh """
-        jmeter -n -t ${testArgs.pathToTest} -l ${testArgs.pathToTest}.result
-        cat ${testArgs.pathToTest}.result
+        jmeter -n -t ${testArgs.pathToTest} -l ${testArgs.pathToTest}_result.jtl
+        cat ${testArgs.pathToTest}_result.jtl
     """
 }
